46351fb7847c4ded943fc8b1e1ee6a5650a711ad
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-02-22 17:14:30 +0200

    Fix the import brackets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { HeaderView } from "./components/HeaderView.jsx";
+import HeaderView from "./components/HeaderView.jsx";
+import LoadingView from "./components/LoadingView.jsx";
 
 function App() {
   return (
