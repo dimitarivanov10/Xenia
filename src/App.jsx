a2fa300da289464bf430fdeb2e582ba200014c6e
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-02-22 16:54:49 +0200

    Import the header in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { HeaderView } from "./components/HeaderView.jsx";
 
+function App() {
+  return (
+    <main>
+      <LoadingView />
 
+      <HeaderView />
+    </main>
+  );
+}
