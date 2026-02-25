08ea2e885c20ee0bdbd4702c6c79bbb32a4de4d9
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-02-25 15:47:48 +0200

    Changed the header location

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import LoadingView from "./components/LoadingView.jsx";
+import HeaderView from "./components/HeaderView.jsx"
 
 import FooterView from "./components/FooterView.jsx";
 import HomeView from "./views/HomeView.jsx";
@@ -8,6 +9,8 @@ function App() {
   return (
     <main>
       {/* <LoadingView /> */}
+      <HeaderView />
+
       <HomeView />
       <FooterView />
     </main>
