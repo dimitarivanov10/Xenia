e62c809a22fae72e49401305d73e9f0ea624b704
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-02-26 11:16:01 +0200

    Fix the location of the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import LoadingView from "./components/LoadingView.jsx";
-import HeaderView from "./components/HeaderView.jsx"
-
+import HeaderView from "./components/HeaderView.jsx";
 import FooterView from "./components/FooterView.jsx";
 import HomeView from "./views/HomeView.jsx";
 
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import MainBannerView from "../components/MainBannerView.jsx";
 import VisitCountryView from "../components/VisitCountryView.jsx";
 import CallToActionView from "../components/CalltoActionView.jsx";
-import HeaderView from "../components/HeaderView.jsx";
 
 export default function HomeView() {
   return (
     <main>
-      <HeaderView />
 
       <MainBannerView />
       <VisitCountryView />
