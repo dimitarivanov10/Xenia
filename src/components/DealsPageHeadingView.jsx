29fe78a244fab0e18b9fa7d554f6ebdef32ff2a9
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-03-03 23:27:54 +0200

    Re-factor the deals page heading

---
+++ src/components/DealsPageHeadingView.jsx
@@ -0,0 +1,17 @@
+export default function DealsPageHeadingView() {
+  return (
+    <div className="page-heading">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h4>Discover Our Weekly Offers</h4>
+            <h2>Amazing Prices &amp; More</h2>
+            <div className="border-button">
+              <a href="about.html">Discover More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/views/DealsView.jsx
@@ -0,0 +1,9 @@
+import DealsPageHeadingView from "../components/DealsPageHeadingView.jsx";
+
+export default function DealsView() {
+  return (
+    <main>
+      <DealsPageHeadingView />
+    </main>
+  );
+}
