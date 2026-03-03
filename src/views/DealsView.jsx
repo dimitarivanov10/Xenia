717349d94aa718f33c18b4b1efd97904f114d6a4
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-03-03 23:31:05 +0200

    Re-factor the deals search

--- src/views/DealsView.jsx
+++ src/views/DealsView.jsx
@@ -1,9 +1,11 @@
 import DealsPageHeadingView from "../components/DealsPageHeadingView.jsx";
+import DealsSearchFormView from "../components/DealsSearchFormView.jsx";
 
 export default function DealsView() {
   return (
     <main>
       <DealsPageHeadingView />
+      <DealsSearchFormView />
     </main>
   );
 }
