18ecf4bcee2cbfdb1976d36afca7167cd8305928
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-03-05 23:40:18 +0200

    refactor: rebuild the more information segment into a jsx file

---
+++ src/views/ReservationView.jsx
@@ -0,0 +1,11 @@
+import ReservationMoreInfoView from "../components/ReservationMoreInfoView.jsx";
+import ReservationSecondPageHeadingView from "../components/ReservationSecondPageHeadingView.jsx";
+
+export default function ReservationView() {
+  return (
+    <main>
+      <ReservationSecondPageHeadingView />
+      <ReservationMoreInfoView />
+    </main>
+  );
+}
